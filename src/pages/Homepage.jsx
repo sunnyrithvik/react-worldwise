d4160ce85542f674c4fd5ed485d2486339c08d0d
sunnyrithvik <sribhashyamrithvik@gmail.com>
2024-08-12 19:32:04 +0530

    initial setup

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Homepage</h1>
+    </div>
+  );
+}
---
+++ src/pages/Product.jsx
@@ -0,0 +1,10 @@
+import PageNav from "../components/PageNav";
+
+export default function Product() {
+  return (
+    <div>
+      <PageNav />
+      <h1>Product</h1>
+    </div>
+  );
+}
