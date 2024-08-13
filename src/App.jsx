2d71cb76ce6054fb22c8db6585ebca7d79943b86
sunnyrithvik <sribhashyamrithvik@gmail.com>
2024-08-13 10:57:37 +0530

    added pages and comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
+import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
   return (
     <div>
-      <h1> Welcome to Advanced React (React Routing)</h1>
-
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="product" element={<Product />} />
           <Route path="pricing" element={<Pricing />} />
+          <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
